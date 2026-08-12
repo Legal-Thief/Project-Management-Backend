7ea9f106abf9e14b3b107c0b3622c80132896f91
YourGitHubUsername <tanishqpatel650@gmail.com>
2026-08-13 00:05:51 +0530

    Created schema of User and added  bcrypt for password to hash in it

---
+++ src/models/user.models.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema } from "mongoose";
+import brcypt from "brcypt"
+const userSchema = new Schema(
+  {
+    avatar: {
+      type: {
+        url: String,
+        localPath: String,
+      },
+      default: {
+        url: `https://placehold.co/200x200.png`,
+        localPath: "",
+      },
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowsercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshToken: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next()
+        
+    this.password=await brcypt.hash(this.password,10)
+    next()
+})
+
+
+export const User = mongoose.model("User", userSchema);
